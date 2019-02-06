4ef2c5d04150ba008db265f04ba4aa959857a264
TonyCC <cool21540125@gmail.com>
2019-02-07 02:08:48 +0800

    4nd test (frames in game)

--- DemoBoiling/FrameTest/GameTest.cs
+++ DemoBoiling/FrameTest/GameTest.cs
@@ -13,5 +13,27 @@ namespace Boiling.Tests
             game.Add(5);
             Assert.AreEqual(5, game.Score);
         }
+
+        [Test]
+        public void TestTwoThrowsNoMark()
+        {
+            Game game = new Game();
+            game.Add(5);
+            game.Add(4);
+            Assert.AreEqual(9, game.Score);
+        }
+
+        [Test]
+        public void TestFourThrowsNoMark()
+        {
+            Game game = new Game();
+            game.Add(5);
+            game.Add(4);
+            game.Add(7);
+            game.Add(2);
+            Assert.AreEqual(18, game.Score);
+            Assert.AreEqual(9, game.ScoreForFrame(1));
+            Assert.AreEqual(18, game.ScoreForFrame(2));
+        }
     }
 }
